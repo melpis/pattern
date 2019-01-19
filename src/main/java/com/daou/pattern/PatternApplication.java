d55780cf7e177f02d8f030b80b01925bc77f6f05
melpis <melpis10@gmail.com>
2019-01-19 15:30:57 +0900

    sevlet 추가

--- src/main/java/com/daou/pattern/PatternApplication.java
+++ src/main/java/com/daou/pattern/PatternApplication.java
@@ -2,6 +2,16 @@ package com.daou.pattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @SpringBootApplication
 public class PatternApplication {
@@ -9,6 +19,22 @@ public class PatternApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatternApplication.class, args);
 	}
+	@Configuration
+	public class ServletRegistrationConfig {
+		@Bean
+		public ServletRegistrationBean worldServletBean()
+		{
+			return new ServletRegistrationBean(new WorldServlet(),"/world");
+		}
+	}
 
+	public class WorldServlet extends HttpServlet {
+		@Override
+		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			resp.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = resp.getWriter();
+			out.println("World");
+		}
+	}
 }
 
