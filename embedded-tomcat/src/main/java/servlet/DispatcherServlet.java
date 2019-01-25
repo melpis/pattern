1ae2b43c5b1510c8314b35c26b7fb77846e74740
홍성원 <melpis@daou.co.kr>
2019-01-25 16:04:00 +0900

    front controller 마무리

--- embedded-tomcat/src/main/java/servlet/DispatcherServlet.java
+++ embedded-tomcat/src/main/java/servlet/DispatcherServlet.java
@@ -19,14 +19,33 @@ public class DispatcherServlet extends HttpServlet {
         String requestURL = req.getRequestURI();
         String url = requestURL.substring(1);
         HttpServlet httpServlet = null;
-        if ("order".equalsIgnoreCase(url)) {
-            httpServlet = new OrderServlet();
-        } else if("special/order".equalsIgnoreCase(url)) {
-            httpServlet = new SpecialOrderServlet();
+        try {
+            httpServlet = handlerMapping(url);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         if(httpServlet != null){
             httpServlet.service(req,resp);
-            req.getRequestDispatcher("/WEB-INF/"+url+".jsp").forward(req, resp);
+            viewResolver(req, resp, url);
         }
+    }
+
+    private void viewResolver(HttpServletRequest req, HttpServletResponse resp, String url) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/"+url+".jsp").forward(req, resp);
+    }
+
+    private HttpServlet handlerMapping(String url) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        String[] urlToken = url.split("\\/");
+        if(url.contains("/")){
+            StringBuilder stringBuilder = new StringBuilder();
+            for(String className:urlToken){
+                stringBuilder.append(className.substring(0, 1).toUpperCase()).append(className.substring(1));
+            }
+            url = stringBuilder.toString();
+        }else{
+            url = url.substring(0,1).toUpperCase()+url.substring(1);
+        }
+
+        return (HttpServlet)Class.forName("servlet."+url+"Servlet").newInstance();
     }
 }
