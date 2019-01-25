0cbedc32237ed9be63a620908b5a64dc7d638ccc
홍성원 <melpis@daou.co.kr>
2019-01-25 10:41:26 +0900

    front controller

---
+++ embedded-tomcat/src/main/java/servlet/DispatcherServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        name = "dispatch",
+        urlPatterns = {"/"}
+)
+public class DispatcherServlet extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String requestURL = req.getRequestURI();
+        String url = requestURL.substring(1);
+        HttpServlet httpServlet = null;
+        if ("order".equalsIgnoreCase(url)) {
+            httpServlet = new OrderServlet();
+        } else if("special/order".equalsIgnoreCase(url)) {
+            httpServlet = new SpecialOrderServlet();
+        }
+        if(httpServlet != null) httpServlet.service(req,resp);
+    }
+}
--- embedded-tomcat/src/main/java/servlet/OrderServlet.java
+++ embedded-tomcat/src/main/java/servlet/OrderServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "order" ,urlPatterns = "/order")
+
 public class OrderServlet extends HttpServlet {
 
     @Override
