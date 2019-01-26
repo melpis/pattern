0bb2aad34d8b352c218a2cf90f8d98466a798acc
melpis <melpis10@gmail.com>
2019-01-26 23:00:35 +0900

    커밋!

--- springmvc.md
+++ springmvc.md
@@ -5,11 +5,44 @@ DispatcherServlet
 ![](흐름.PNG)
 
 1. HandlerMapping
-    > URL과 요청 정보 기준으로 어떤 핸들러 오브젝트, 즉 컨트롤러를 사용할것인지  결정하는 로직을 담당한다.
+    > URL과 요청 정보 기준으로 어떤 핸들러 오브젝트, 즉 컨트롤러를 사용할것인지 결정하는 로직을 담당한다.
 2. HandlerAdapter
     > 핸들러 매핑으로 선택한 컨트롤러/핸드러를 'DispatcherServlet'이 호출할때 사용하는 어댑터 이다.
+    > 어노테이션 기반에 핸들러 어댑터에서는 리턴 타입 파라미터 사용이 자유롭다 위 맵핑에서는 클래스나 메소드명 정도만 가지고 있고
+    > 실제 실행은 여기서 !!
 3. HandlerExceptionResolver
 4. ViewResolver
 5. LocaleResolver
 6. ThemeResolver
 7. RequestToViewNameTranslator
+
+
+DispatcherServlet 
+> doService > doDispatch
+
+processedRequest = checkMultipart(request);
+				multipartRequestParsed = (processedRequest != request);
+
+				// Determine handler for the current request.
+				// url과 맵핑 되는 메소드 찾기
+				HandlerExecutionChain mappedHandler = getHandler(processedRequest);
+				
+				if (mappedHandler == null) {
+					noHandlerFound(processedRequest, response);
+					return;
+				}
+
+				// Determine handler adapter for the current request.
+				HandlerAdapter ha = getHandlerAdapter(mappedHandler.getHandler());
+                if (!mappedHandler.applyPreHandle(processedRequest, response)) {
+                            return;
+                }
+			
+				// Actually invoke the handler.
+				// 실제 실행 코드
+				mv = ha.handle(processedRequest, response, mappedHandler.getHandler());
+
+				applyDefaultViewName(processedRequest, mv);
+				mappedHandler.applyPostHandle(processedRequest, response, mv);
+				
+				processDispatchResult(processedRequest, response, mappedHandler, mv, dispatchException);
--- src/main/java/com/daou/pattern/PatternApplication.java
+++ src/main/java/com/daou/pattern/PatternApplication.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @SpringBootApplication
 @RestController
@@ -23,44 +22,9 @@ public class PatternApplication {
 		SpringApplication.run(PatternApplication.class, args);
 	}
 
-	public class ServletRegistrationConfig {
-		public ServletRegistrationBean worldServletBean()
-		{
-			return new ServletRegistrationBean(new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String requestURL = req.getRequestURI();
-					String substring = requestURL.substring(1, requestURL.length());
-					System.out.println(substring);
-					try {
-						HttpServlet httpServlet = (HttpServlet)Class.forName("com.daou.pattern.Servletword").newInstance();
-						httpServlet.service(req,resp);
-					} catch (InstantiationException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						e.printStackTrace();
-					} catch (ClassNotFoundException e) {
-						e.printStackTrace();
-					}
-
-//					if("/world".equalsIgnoreCase(requestURL)){
-//						resp.setContentType("text/html; charset=UTF-8");
-//						PrintWriter out = resp.getWriter();
-//						out.println("World");
-//					}else{
-//						resp.setContentType("text/html; charset=UTF-8");
-//						PrintWriter out = resp.getWriter();
-//						out.println("no");
-//					}
-
-				}
-			}, "/");
-		}
-	}
-
 	@RequestMapping("/test")
 	public String test(){
-		return "";
+		return "test";
 	}
 
 
